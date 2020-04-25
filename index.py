1cd41fe68823b1a6a29b6f6d1dc30cf65b78d60b
Felipe Rezende <feliperezende.srz@gmail.com>
2020-04-25 10:05:55 -0300

    Escutando e decifrando o que foi dito

---
+++ index.py
@@ -0,0 +1,28 @@
+﻿# -*- coding: utf-8 -*-
+
+import speech_recognition as sr
+#Funcao responsavel por ouvir e reconhecer a fala
+def ouvir_microfone():
+    #Habilita o microfone para ouvir o usuario
+    microfone = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        #Chama a funcao de reducao de ruido disponivel na speech_recognition
+        microfone.adjust_for_ambient_noise(source)
+        #Avisa ao usuario que esta pronto para ouvir
+        print("Diga alguma coisa: ")
+        #Armazena a informacao de audio na variavel
+        audio = microfone.listen(source)
+
+    try:
+        #Passa o audio para o reconhecedor de padroes do speech_recognition
+        frase = microfone.recognize_google(audio, language='pt-BR')
+        #Apos alguns segundos, retorna a frase falada
+        print("Você disse: " + frase.encode('utf8'))
+        #Caso nao tenha reconhecido o padrao de fala, exibe esta mensagem
+    except LookupError:
+        print("Não entendi")
+
+    return frase
+
+ouvir_microfone()
