c4911358d399771967c37fcaf9a2b1d032b84c81
Felipe Rezende <feliperezende.srz@gmail.com>
2020-04-25 16:50:39 -0300

    Refatorando código e redefinindo os arquivos

--- index.py
+++ index.py
@@ -3,54 +3,20 @@
 import os
 import re
 import sys
-import speech_recognition as sr
-from gtts import gTTS
-from playsound import playsound
-sys.path.append('/home/felipe/Workspace/git/voice-control/models/')
 
-import command as commandModule
+path = os.getcwd()
+sys.path.append(path + '/lib/')
+
+import command
+import comunication
 
 def clear():
     os.system('cls' if os.name=='nt' else 'clear')
 
-def search_command(request):
-    for cmd in commandModule.commands:
-        if (re.search(cmd.key, request, re.IGNORECASE)):
-            os.system(cmd.content)
-
-def speak_word(word):
-    tts = gTTS(word,lang='pt-br')
-
-    tts.save('assets/speak.mp3')
-
-    playsound('assets/speak.mp3')
-
-def listen_microfone(listenMessage = "Pode falar"):
-    microfone = sr.Recognizer()
-
-    with sr.Microphone() as source:
-        microfone.adjust_for_ambient_noise(source)
-        
-        # clear()
-        print(listenMessage)
-        
-        audio = microfone.listen(source)
-    try:
-        
-        word = microfone.recognize_google(audio, language='pt-BR')
-        word = word.encode('utf8')
-        
-        print("Você disse: " + word)
-        
-    except LookupError:
-        print("Não entendi")
-
-    return word
-
-word = listen_microfone()
+word = comunication.listen_microfone()
 
 if (re.search('olá nala', word, re.IGNORECASE)):
-    speak_word('Oi, estou aqui para ajudar, basta pedir')
+    comunication.speak_word('Oi, estou aqui para ajudar, basta pedir')
     print("Pode pedir:")
-    pedido = listen_microfone()
-    search_command(pedido)
+    request = comunication.listen_microfone()
+    command.search_command(request)
--- models/command.py
+++
@@ -1,8 +0,0 @@
-class command:  
-    def __init__(self, key, content):  
-        self.key = key  
-        self.content = content 
-
-commands = []  
-commands.append( command('Abrir Chrome', 'google-chrome') ) 
-commands.append( command('Abrir Code', 'code') ) 
