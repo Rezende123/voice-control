9dca6963f2987d6d96e5e859ef38f45edd0f4485
Felipe Rezende <feliperezende.srz@gmail.com>
2020-04-26 19:58:52 -0300

    Habilitando busca no navegador

--- modules/command.py
+++ modules/command.py
@@ -2,6 +2,7 @@ import re
 import os
 from difflib import SequenceMatcher
 from fuzzywuzzy import fuzz
+import webbrowser
 
 class command: 
     def __init__(self, key, content):  
@@ -12,11 +13,42 @@ commands = []
 commands.append( command('Abrir Chrome', 'google-chrome') ) 
 commands.append( command('Abrir Code', 'code') ) 
 
-def similar(a, b):
-    return SequenceMatcher(None, a.lower(), b.lower()).ratio()
+def getWords(word):
+    return word.split(" ")
+
+def makeParams(words):
+    params = ''
+
+    for word in words:
+        if (params == ''):
+            params = word
+        else:
+            params = params + '%20' + word
+    
+    return params
 
 def search_command(request):
     for cmd in commands:
-        if (fuzz.partial_ratio(cmd.key, request) >= 80):
+        if (
+            re.search('buscar', request, re.IGNORECASE) or
+            re.search('procurar', request, re.IGNORECASE)            
+            ):
+
+            words = getWords(request.upper())
+
+            if 'BUSCAR' in words:
+                words.remove('BUSCAR')
+            elif 'PROCURAR' in words:
+                words.remove('PROCURAR')
+
+            params = makeParams(words)
+
+            url = "https://www.google.com.tr/search?q={}".format(params)
+            webbrowser.open_new_tab(url)
+            
+
+        elif (fuzz.partial_ratio(cmd.key, request) >= 80):
             os.system(cmd.content)
             return
+
+search_command('buscar Globo Esporte')
